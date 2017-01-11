e30c7d961e059a91d9a1d572c17a0536f2ad226d
Cathi <c.broermann@reply.de>
2017-01-11 13:01:00 +0100

    test commit catih

---
+++ gittest1234/src/main/java/gittest/gittest1234/App.java
@@ -0,0 +1,13 @@
+package gittest.gittest1234;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
